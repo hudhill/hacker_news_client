207cc8270d20d362ef252de631e98369bb670b4d
hudhill <sam.hudhill@gmail.com>
2021-07-27 13:01:30 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import StoryContainer from './containers/StoryContainer';
+import './App.css'
 
 function App() {
 
--- src/containers/StoryContainer.js
+++ src/containers/StoryContainer.js
@@ -1,7 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import StoryList from '../components/StoryList';
 import SearchBar from '../components/SearchBar';
-import "./StoryContainer.css"
 
 const StoryContainer = () => {
 
